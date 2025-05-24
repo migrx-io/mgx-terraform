f46e7ec01c50105bd52ea2372054b8b538961fba
anatolii2makaroff <anatolii.makaroff@gmail.com>
2025-05-23 22:56:49 -0400

    fx output

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -5,6 +5,12 @@ output "bastion_public_ip" {
 }
 
 output "storage_node_private_ips" {
-  description = "Private IPs of the storage nodes"
-  value       = join("\n", [for instance in aws_instance.storage_node : instance.private_ip])
+  description = "Private IPs of the storage nodes grouped by pool"
+  value = {
+    for pool_name in keys(var.storage_pools) :
+    pool_name => [
+      for instance_key, instance in aws_instance.storage_node :
+      instance.private_ip if startswith(instance_key, "${pool_name}-")
+    ]
+  }
 }
