664149e596beb6bdfd36d8ce3d7d39b39a341e0b
anatolii2makaroff <anatolii.makaroff@gmail.com>
2025-08-13 18:59:45 -0400

    update

--- scripts/setup-storage.sh
+++ scripts/setup-storage.sh
@@ -8,12 +8,11 @@ MGX_VAR_DIR=/var/lib/migrx
 sh ./mgx-bootstrap-deb.sh
 
 # 2. Generate mgx-id and mgx-hosts
-if [ ! -f ${MGX_VAR_DIR}/mgx-id ]; then                                         
-    uuidgen > ${MGX_VAR_DIR}/mgx-id                                             
-fi                                                                              
-
+# if [ ! -f ${MGX_VAR_DIR}/mgx-id ]; then                                         
+#     uuidgen > ${MGX_VAR_DIR}/mgx-id                                             
+# fi                                                                              
 # set all hosts for pool
-echo "" >> ${MGX_VAR_DIR}/mgx-hosts
+# echo "" >> ${MGX_VAR_DIR}/mgx-hosts
 
 
 # 1. Copy and modify env files
