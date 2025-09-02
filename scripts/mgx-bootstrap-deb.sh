45e0c282555b328888331fd6b80c48a8ee162ba9
anatolii2makaroff <anatolii.makaroff@gmail.com>
2025-09-02 01:00:47 -0400

    rm retry for update

--- scripts/mgx-bootstrap-deb.sh
+++ scripts/mgx-bootstrap-deb.sh
@@ -46,9 +46,7 @@ echo "STEP 1. Create DEB repo.."
 echo ""
 
 echo "deb [trusted=yes] https://repo.migrx.io/DEBS/ migrx main" | tee /etc/apt/sources.list.d/migrx.list
-
-# retry in case if repo is slow
-for i in 1 2 3; do apt-get update && break || sleep 10; done
+apt-get update
 
 echo "STEP 2. Install packages.."
 echo ""
