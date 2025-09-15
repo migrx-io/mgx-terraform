090c53c4b79c56ca894f131767a7b9a2ef85edc0
anatolii2makaroff <anatolii.makaroff@gmail.com>
2025-09-14 22:54:13 -0400

    add storage plugin

--- scripts/mgx-plugins-deb.sh
+++ scripts/mgx-plugins-deb.sh
@@ -11,3 +11,4 @@ apt install -t migrx -y mgx-plgn-aaa
 apt install -t migrx -y mgx-plgn-notif
 apt install -t migrx -y mgx-plgn-services
 apt install -t migrx -y mgx-plgn-cache
+apt install -t migrx -y mgx-plgn-storage
